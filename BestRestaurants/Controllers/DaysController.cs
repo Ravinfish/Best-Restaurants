d0a9fb66bd5876977c2a05a9b95237b5c8dce370
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-02-08 10:21:49 -0800

    add service action and view to day
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- BestRestaurants/Controllers/DaysController.cs
+++ BestRestaurants/Controllers/DaysController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BestRestaurants.Controllers;
 
-public class DaysController : Controller 
+public class DaysController : Controller
 {
   private readonly BestRestaurantsContext _db;
 
@@ -18,5 +18,33 @@ public class DaysController : Controller
   public ActionResult Index()
   {
     return View(_db.Days.ToList());
+  }
+
+  public ActionResult Details(int id)
+  {
+    Day thisDay = _db.Days
+    .Include(day => day.JoinEntities)
+    .ThenInclude(join => join.Service)
+    .FirstOrDefault(day => day.DayId == id);
+    return View(thisDay);
+  }
+  public ActionResult AddService(int id)
+  {
+    Day thisDay = _db.Days.FirstOrDefault(days => days.DayId == id);
+    ViewBag.ServiceId = new SelectList(_db.Services, "ServiceId", "Type");
+    return View(thisDay);
+  }
+  [HttpPost]
+  public ActionResult AddService(Day day, int serviceId)
+  {
+    #nullable enable
+    DayService? joinEntity = _db.DayServices.FirstOrDefault(join => (join.ServiceId == serviceId && join.DayId == day.DayId));
+    #nullable disable
+    if (joinEntity == null && serviceId != 0)
+    {
+      _db.DayServices.Add(new DayService() { ServiceId = serviceId, DayId = day.DayId });
+      _db.SaveChanges();
+    }
+    return RedirectToAction("Details", new { id = day.DayId });
   }
 }
